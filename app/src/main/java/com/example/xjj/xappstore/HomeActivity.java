325fe3eaeb3dcc9b43ba0a9b22e2c58e0e90d902
alterjx <alterjx@163.com>
2018-04-15 11:54:41 +0800

    第一次修改

--- app/src/main/java/com/example/xjj/xappstore/HomeActivity.java
+++ app/src/main/java/com/example/xjj/xappstore/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.xjj.xappstore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
     }
 }
